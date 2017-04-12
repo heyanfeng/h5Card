6052d65cc3139fb36c860f5b1d154cc554d9af5a
heyanfeng <528455318@qq.com>
2017-04-12 18:13:22 +0800

    begin

---
+++ src/components/index/index.js
@@ -0,0 +1,23 @@
+import '../../../static/css/reset.css'
+import '../../../static/css/index.css'
+export default {
+  name: 'index',
+  data () {
+    return {
+      holidayLists: [
+        {
+          img: '',
+          text: '新年送祝福，就得玩点特别哒！'
+        },
+        {
+          img: '',
+          text: '元旦贺卡'
+        },
+        {
+          img: '',
+          text: '圣诞节贺卡'
+        }
+      ]
+    }
+  }
+}
