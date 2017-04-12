34dc35184693e02ffb5a88d21281c8dd64e3cc8b
heyanfeng <528455318@qq.com>
2017-04-12 22:08:44 +0800

    add index

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -6,18 +6,48 @@ export default {
     return {
       holidayLists: [
         {
-          img: '',
+          img: '../../../static/imgs/index-bg-newYear.jpg',
           text: '新年送祝福，就得玩点特别哒！'
         },
         {
-          img: '',
+          img: '../../../static/imgs/index-bg-yuandan.jpg',
           text: '元旦贺卡'
         },
         {
-          img: '',
+          img: '../../../static/imgs/index-bg-christmas.jpg',
           text: '圣诞节贺卡'
+        },
+        {
+          img: '../../../static/imgs/index-bg-dongzhi.jpg',
+          text: '冬至贺卡'
+        },
+        {
+          img: '../../../static/imgs/index-bg-birthday.jpg',
+          text: '生日贺卡'
+        },
+        {
+          img: '../../../static/imgs/index-bg-thank.jpg',
+          text: '感恩节贺卡'
+        },
+        {
+          img: '../../../static/imgs/index-bg-nationalDay.jpg',
+          text: '国庆节贺卡'
+        },
+        {
+          img: '../../../static/imgs/index-bg-midAutumn.jpg',
+          text: '中秋节贺卡'
         }
       ]
+    }
+  },
+  methods: {
+    selectTemplate (index) {
+      this.$router.push({
+        path: 'cardDetails',
+        query: {
+          cardId: index
+        }
+      })
     }
   }
 }
